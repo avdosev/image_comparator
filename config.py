53b76cdf1e6ec0e98725fb0ecab8a4c2e4fbc17c
avdosev <avdosev1@yandex.ru>
2020-06-04 20:34:16 +0400

    Результаты стали более правдеподобными

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ models_path = './models'
 input_shape = (300, 300, 3)
 images_folder = './dataset/images'
 images_count = 718
-epoch = 25
+epoch = 20
 batch = 10
--- networks.py
+++ networks.py
@@ -6,7 +6,8 @@ def get_model(input_shape, emb_size, num_imgs):
     return keras.Sequential([
         keras.applications.MobileNetV2(input_shape=input_shape, weights='imagenet', include_top=False),
         keras.layers.GlobalAveragePooling2D(),
-        keras.layers.Dense(emb_size),
+        keras.layers.Dense(emb_size, activation='relu'),
+        keras.layers.BatchNormalization(),
         keras.layers.Dense(num_imgs, activation='relu'),
         keras.layers.Softmax(input_shape=(num_imgs,))
     ])
